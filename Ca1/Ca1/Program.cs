ffabff6954a2908f293dce5112b7023167a46158
Piotr Golkowski <47738716+S00188468@users.noreply.github.com>
2019-11-04 12:33:36 +0000

    CA1

---
+++ Ca1/Ca1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ca1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Song song1 = new Song("ED SHEERAN", "BLYATFUL PEOPLE", 3.15);
+            Song song2 = new Song("REGARD", "RIDE IT", 3.37);
+            Song song3 = new Song("TONES & I", "DANCE MONKEY", 4.20);
+            Song song4 = new Song("POST MALONE", "CIRCLES", 3.25);
+            Song song5 = new Song("TYLER THE CREATOR", "SHE", 4.13);
+
+            List<Song> Playlist = new List<Song>();
+            Playlist.Add(song1);
+            Playlist.Add(song2);
+            Playlist.Add(song3);
+            Playlist.Add(song4);
+            Playlist.Add(song5);
+
+
+        }
+        static void DisplayPlaylist(List<Song> Playlist)
+        {
+            for (int i = 0; i < Playlist.Count; i++)
+            {
+                Console.WriteLine("{0-15}{1-15}{2-15}{3,-15}", Playlist[i].artist, Playlist[i].title, Playlist[i].duration);
+            }
+
+        }
+
+
+    }
+    }
---
+++ Ca1/Ca1/Song.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ca1
+{
+    
+    class Song
+    {
+        enum Genre
+        {
+            Rock,
+            Pop,
+            Dance,
+            Other
+        }
+       public string title { get; set; }
+       public string artist { get; set; }
+       public double duration { get; set; }
+        //Genre genre { get;}
+        public  Song(string title, string artist, double duration)
+        {
+            this.title = title;
+            this.artist = artist;
+            this.duration = duration;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0,-10} {1,-10} {2,-10} ", artist, title, duration);
+        }
+       
+    }
+}
